961a456f6074f53dee7c40b8f67bcfbd7b346ecf
Bruno Almeida <bdalmeida@live.com>
2019-09-18 17:35:44 -0300

    Consulta simples com query methods

--- src/main/java/com/brunoalmeida/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/brunoalmeida/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.brunoalmeida.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.brunoalmeida.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	//query methods
+	List <Post> findByTitleContainingIgnoreCase(String text);
 }
